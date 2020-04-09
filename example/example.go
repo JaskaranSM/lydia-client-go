556cec62ba6be95461a831be5bdc1d4e16c07975
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2020-04-09 15:51:29 +0530

    example: update

--- example/example.go
+++ example/example.go
@@ -5,16 +5,16 @@ import (
 	"github.com/jaskaranSM/lydia-client-go/lydia"
 )
 
-unc main(){
-	var API_KEY = "<your_api_key>"
-	var input string
+func main(){
+    var API_KEY = "<your_api_key>"
+    var input string
     client := lydia.NewClient(API_KEY)
     res,err := client.CreateSession()
     if err != nil {
     	fmt.Println(err)
     }
-	fmt.Scanln(&input)
-	out,err := client.ThinkThought(res.Payload.SessionId,input)
+    fmt.Scanln(&input)
+    out,err := client.ThinkThought(res.Payload.SessionId,input)
     if err != nil {
     	fmt.Println(err)
     }
